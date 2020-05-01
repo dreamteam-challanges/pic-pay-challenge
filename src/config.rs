6f55521f59fd5e4e0eaac30753f7351c26a7bc20
Julia Naomi <jnboeira@outlook.com>
2020-05-01 20:55:00 -0300

    using macros to define path

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,6 @@ use actix_web::web;
 
 pub fn routes(config: &mut web::ServiceConfig) {
     config.service(
-        web::scope("/")
-            .service(
-                web::scope("users")
-                    .route("", web::post().to(users::create))
-            )
+        users::create
     );
 }
--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -1,6 +1,8 @@
-use actix_web::{web, Responder, HttpResponse};
+use actix_web::{web, post, Responder, HttpResponse};
 use crate::schemata::User;
 
+
+#[post("/users")]
 pub async fn create(info: web::Json<User>) -> impl Responder {
     HttpResponse::Created().body("HAHAHAHAHHA")
 }
