95bf0cf479678c63435f8d1a9a186b69e3a61a8c
Otávio Pace <otaviopp8@gmail.com>
2020-05-01 22:15:02 -0300

    docker-compose: add postgres service and migrations

---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    users (id) {
+        id -> Uuid,
+        name -> Varchar,
+        phone -> Varchar,
+        email -> Varchar,
+        cpf -> Varchar,
+        password -> Varchar,
+    }
+}
